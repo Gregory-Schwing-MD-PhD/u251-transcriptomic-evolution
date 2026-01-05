31a73c14b8b14bcbb09a95184eb1ef26b44de9d3
Gregory J. Schwing <go2432@wayne.edu>
2026-01-05 18:45:32 -0500

    update rnaseq

--- run_rnaseq_pdx.sh
+++ run_rnaseq_pdx.sh
@@ -26,10 +26,13 @@ mkdir -p $XDG_RUNTIME_DIR
 nextflow run nf-core/rnaseq \
     -r 3.22.2 \
     -profile singularity \
-    --input ANALYSIS/samplesheet.csv \
-    --outdir ANALYSIS/results \
-    --genome GRCh38 \
-    --bbsplit_fasta_list ANALYSIS/bbsplit.csv \
+    --input "$(pwd)/ANALYSIS/samplesheet.csv" \
+    --outdir "$(pwd)/ANALYSIS/results" \
+    --fasta "$(pwd)/ANALYSIS/refs/human/GRCh38.primary_assembly.genome.fa.gz" \
+    --gtf "$(pwd)/ANALYSIS/refs/human/GRCh38.primary_assembly.annotation.gtf.gz" \
+    --bbsplit_fasta_list "$(pwd)/ANALYSIS/bbsplit.csv" \
     --skip_bbsplit false \
     --save_bbsplit_reads \
+    --max_cpus 16 \
+    --max_memory '64.GB' \
     -resume
